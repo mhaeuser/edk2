87b3d2de23505feb627506a008f9713fda7fd65a
Marvin Häuser <mhaeuser@posteo.de>
2021-08-08 08:58:32 +0200

    ArmPkg/DefaultExceptionHandlerLib: Mandatory LoadedImage for DebugImageInfoTable
    To make parsing DebugImageInfoTable easier and safer, require the
    LoadedImage protocol instance to be valid for every NormalImage
    entry.
    
    Cc: Leif Lindholm <leif@nuviainc.com>
    Cc: Ard Biesheuvel <ardb+tianocore@kernel.org>
    Cc: Vitaly Cheptsov <vit9696@protonmail.com>
    Signed-off-by: Marvin Häuser <mhaeuser@posteo.de>

--- ArmPkg/Library/DefaultExceptionHandlerLib/DefaultExceptionHandlerUefi.c
+++ ArmPkg/Library/DefaultExceptionHandlerLib/DefaultExceptionHandlerUefi.c
@@ -52,8 +52,9 @@ GetImageName (
   Address = (CHAR8 *)(UINTN)FaultAddress;
   for (Entry = 0; Entry < DebugTableHeader->TableSize; Entry++, DebugTable++) {
     if (DebugTable->ImageInfoType != NULL) {
-      if ((*DebugTable->ImageInfoType == EFI_DEBUG_IMAGE_INFO_TYPE_NORMAL) &&
-          (DebugTable->NormalImage->LoadedImageProtocolInstance != NULL)) {
+      if (*DebugTable->ImageInfoType == EFI_DEBUG_IMAGE_INFO_TYPE_NORMAL) {
+        ASSERT (gDebugTable->NormalImage->LoadedImageProtocolInstance != NULL);
+
         if ((Address >= (CHAR8 *)DebugTable->NormalImage->LoadedImageProtocolInstance->ImageBase) &&
             (Address <= ((CHAR8 *)DebugTable->NormalImage->LoadedImageProtocolInstance->ImageBase + DebugTable->NormalImage->LoadedImageProtocolInstance->ImageSize))) {
           *ImageBase = (UINTN)DebugTable->NormalImage->LoadedImageProtocolInstance->ImageBase;
